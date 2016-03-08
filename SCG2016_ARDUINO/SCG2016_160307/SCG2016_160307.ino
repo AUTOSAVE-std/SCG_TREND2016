1bbec1cafee2a19636c2744bce8bc5afe1a91b2c
หมี โหด <eoeoeseseoes@eoeos.com>
2016-03-09 01:37:08 +0700

    change 2
    input more sensor

--- SCG2016_ARDUINO/SCG2016_160307/SCG2016_160307.ino
+++ SCG2016_ARDUINO/SCG2016_160307/SCG2016_160307.ino
@@ -1,35 +1,95 @@
  
 
 
-#define echoPin 7 // Echo Pin
-#define trigPin 8 // Trigger Pin
+#define echoPin 2 // Echo Pin
+#define trigPin 3 // Trigger Pin
+
+#define echoPin1 4 // Echo Pin
+#define trigPin1 5 // Trigger Pin
+
+#define echoPin2 6 // Echo Pin
+#define trigPin2 7 // Trigger Pin
+
+#define echoPin3 8 // Echo Pin
+#define trigPin3 9 // Trigger Pin
+
 #define LEDPin 13 // Onboard LED
 
 int maximumRange = 35; // Maximum range needed
 int minimumRange = 0; // Minimum range needed
-long duration, distance; // Duration used to calculate distance
+long one, two, tree, four;
+long distance; // Duration used to calculate distance
 
 void setup() {
  Serial.begin (9600);
  pinMode(trigPin, OUTPUT);
  pinMode(echoPin, INPUT);
+ 
+ pinMode(trigPin1, OUTPUT);
+ pinMode(echoPin1, INPUT);
+ 
+ pinMode(trigPin2, OUTPUT);
+ pinMode(echoPin2, INPUT);
+ 
+ pinMode(trigPin3, OUTPUT);
+ pinMode(echoPin3, INPUT);
+ 
  pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+}
+
+void AUTOSAVEsensor(int xtrigPin, int xechoPin) {
+ digitalWrite(xtrigPin, LOW);
+ delayMicroseconds(2); 
+ digitalWrite(xtrigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(xtrigPin, LOW);
+ int xduration = pulseIn(xechoPin, HIGH);
+ int xdistance = xduration/58.2;
+ distance = xdistance;
+ 
 }
 
 void loop() {
 /* The following trigPin/echoPin cycle is used to determine the
  distance of the nearest object by bouncing soundwaves off of it. */ 
- digitalWrite(trigPin, LOW); 
- delayMicroseconds(2); 
+AUTOSAVEsensor(trigPin,echoPin);
+one = distance;
 
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10); 
+AUTOSAVEsensor(trigPin1,echoPin1);
+two = distance;
+
+AUTOSAVEsensor(trigPin2,echoPin2);
+tree = distance;
+
+AUTOSAVEsensor(trigPin3,echoPin3);
+four = distance;
+
+// digitalWrite(trigPin, LOW); 
+// digitalWrite(trigPin1, LOW); 
+// digitalWrite(trigPin2, LOW); 
+// digitalWrite(trigPin3, LOW); 
+// delayMicroseconds(2); 
+
+// digitalWrite(trigPin, HIGH);
+// digitalWrite(trigPin1, HIGH);
+// digitalWrite(trigPin2, HIGH);
+// digitalWrite(trigPin3, HIGH);
+// delayMicroseconds(10); 
+// 
+// digitalWrite(trigPin, LOW);
+// digitalWrite(trigPin1, LOW);
+// digitalWrite(trigPin2, LOW);
+// digitalWrite(trigPin3, LOW);
  
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH);
- 
+// duration = pulseIn(echoPin, HIGH);
+// duration1 = pulseIn(echoPin1, HIGH);
+// duration2 = pulseIn(echoPin2, HIGH);
+// duration3 = pulseIn(echoPin3, HIGH);
  //Calculate the distance (in cm) based on the speed of sound.
- distance = duration/58.2;
+// distance = duration/58.2;
+// distance1 = duration1/58.2;
+// distance2 = duration2/58.2;
+// distance3 = duration3/58.2;
  
  if (distance >= maximumRange || distance <= minimumRange){
  /* Send a negative number to computer and Turn LED ON 
@@ -39,10 +99,43 @@ void loop() {
  else {
  /* Send the distance to the computer using Serial protocol, and
  turn LED OFF to indicate successful reading. */
- Serial.println(distance);
+ Serial.println('g'+distance);
  digitalWrite(LEDPin, LOW); 
  }
+
+// MODEL START ________________________________
+// if (distance1 >= maximumRange || distance1 <= minimumRange){
+//  digitalWrite(LEDPin, HIGH); 
+// }
+// else {
+//   Serial.println('r'+distance1);
+//   digitalWrite(LEDPin, LOW); 
+// }
+//
+//
+// 
+//  if (distance2 >= maximumRange || distance2 <= minimumRange){
+//  digitalWrite(LEDPin, HIGH); 
+// }
+// else {
+//   Serial.println('e'+distance2);
+//   digitalWrite(LEDPin, LOW); 
+// }
+//
+//
+// 
+//  if (distance3 >= maximumRange || distance3 <= minimumRange){
+//  digitalWrite(LEDPin, HIGH); 
+// }
+// else {
+//   Serial.println('s'+distance3);
+//   digitalWrite(LEDPin, LOW); 
+// }
+ 
  
  //Delay 100ms before next reading.
  delay(100);
 }
+
+
+
--- SCG2016_processing/SCG2016_130307/SCG2016_130307.pde
+++ SCG2016_processing/SCG2016_130307/SCG2016_130307.pde
@@ -36,12 +36,20 @@ void setup() {
   String portName = Serial.list()[5]; 
   myPort = new Serial(this, portName, 9600);
   myPort.bufferUntil('\n'); // Trigger a SerialEvent on new line
- 
-  oscP5 = new OscP5(this,portToListenTo);
+
+  OscProperties myProperties = new OscProperties();
+  myProperties.setDatagramSize(100000); 
+  
+  
+  oscP5 = new OscP5(this,myProperties);
+  
+  
   myRemoteLocation = new NetAddress(ipAddressToSendTo, portToSendTo);  
+  
   myBundle = new OscBundle();
   myMessage = new OscMessage("/"); 
   myMessage.add("0");
+
 }
 
 /* ---------------------serialEvent ---------------------------*/
@@ -53,69 +61,22 @@ void serialEvent(Serial cPort){
  if(comPortString != null) {
  comPortString = trim(comPortString);
  int recieveddistance = (Integer.parseInt(comPortString));
- //print(recieveddistance);
- //print('\n');
- //(Integer.parseInt(comPortString))
+ 
 
 if( recieveddistance <= 10 && recieveddistance >= 0){
     myMessage.setAddrPattern("/layer2/clip1/connect");
     myMessage.add(1);
     myBundle.add(myMessage);
-   send = true;
+    send = true;
+
 }
-  if (send)
-    oscP5.send(myBundle, myRemoteLocation);
+if(send) {
+  oscP5.send(myBundle, myRemoteLocation); 
+}
   myBundle.clear();
  }
 }
 
 void draw() {
-
-  //background(0);  
-  
-  ////draw some rectangles and text
-  //fill(255);
-  //rect(1, 1, width-3, height-103);
-  //fill(0);
-  //text("hold mouse to rotate", 5, 24);
-
-  //fill(200);
-  //rect(1, height-100, width-3, 98);
-  //fill(0);
-  //text("hold mouse to scrub", 5, 400);
-  
-  ////calculate x and y pos in the range 0.0 - 1.0
-  //float xpos = (float) mouseX / (float) width;
-  //float ypos = (float) mouseY / (float) height;
-  
-  //boolean send = false;
- 
-
-  ////check if we need to send a rotate messages  
-  //if (mouseDown && mouseY < height-100) {
-  
-  // myMessage.setAddrPattern("/layer1/clip1/connect");
-  // myMessage.add(ypos);
-  // myBundle.add(myMessage);
-  // myMessage.clear();
-    
-  // send = true;
-    
-  //}
-  
-  ////check if a scrubbing message should be send
-  //if ( mouseDown && mouseY > height-100 ) {
-  // myMessage.setAddrPattern("/layer3/clip1/connect");
-  // myMessage.add(xpos);
-  // myBundle.add(myMessage);
-  // myMessage.clear();
-  // send = true;
-  //}
-  
-  ///* send the bundle*/
-  //if (send)
-  // oscP5.send(myBundle, myRemoteLocation); 
-  
-  //myBundle.clear();
  
 }
